b879f73e30b8bd34af6b9ffc2a5ce526785e9094
snstrauss <zahavi.guy.85@gmail.com>
2020-06-24 13:37:40 +0300

    added link to social media (facebook, youtube)

--- src/components/song-viewer/song-viewer.jsx
+++ src/components/song-viewer/song-viewer.jsx
@@ -3,9 +3,10 @@ import S from './song-viewer.module.scss';
 
 import Video from './partials/video/video';
 import Title from './partials/title/title';
+import SongLink from './partials/song-link/song-link';
 import SocialLink from './partials/social-link/social-link';
 
-export default function SongViewer({ song: { image, links, title, video, artist } }){
+export default function SongViewer({ song: { image, links: { song: songLinks, social }, title, video, artist } }){
 
     const backgroundStyle = {
         backgroundImage: `url(${image})`
@@ -19,9 +20,18 @@ export default function SongViewer({ song: { image, links, title, video, artist 
                 <Title title={title} artist={artist} />
 
                 {
-                    links.map((link) => (
-                        <SocialLink key={link} url={link} title={title}/>
+                    songLinks.map((link) => (
+                        <SongLink key={link} url={link}/>
                     ))
+                }
+                {
+                    <div className={S.socialContainer}>
+                        {
+                            social.map((link) => (
+                                <SocialLink key={link} url={link}/>
+                            ))
+                        }
+                    </div>
                 }
             </main>
         </div>
