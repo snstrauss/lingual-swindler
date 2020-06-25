cfe89561dbba5cbbc65484cdb64e122c5d4fca72
snstrauss <zahavi.guy.85@gmail.com>
2020-06-25 12:28:50 +0300

    new design for social links.

--- src/components/song-viewer/partials/social-link/social-link.jsx
+++ src/components/song-viewer/partials/social-link/social-link.jsx
@@ -3,13 +3,18 @@ import S from './social-link.module.scss';
 import { getHostFromUrl } from '../../../../services/utils';
 import BrandIcon from '../../../iconify/brand-icon';
 import LinkTo from '../../../utils/link-to/link-to';
+import { brandColors } from '../../../../services/brand.service';
 
 export default function SocialLink({ url }){
 
     const networkName = getHostFromUrl(url);
 
+    const style = {
+        "--color": brandColors[networkName]
+    };
+
     return (
-        <LinkTo url={url} className={S.container}>
+        <LinkTo url={url} className={S.container} customStyle={style}>
             <BrandIcon brand={networkName}/>
         </LinkTo>
     )
--- src/components/utils/link-to/link-to.jsx
+++ src/components/utils/link-to/link-to.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function LinkTo({ url, children, className }){
+export default function LinkTo({ url, children, className, customStyle }){
 
     return (
-        <a href={url} target="_blank" rel="noreferrer" className={className} >
+        <a href={url} target="_blank" rel="noreferrer" className={className} style={customStyle}>
             {children}
         </a>
     )
